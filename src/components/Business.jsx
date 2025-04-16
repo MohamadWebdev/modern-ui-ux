4e266c085eea4e14dc1e707ea5a91ddd318f9dae
MohamadWebdev <mohamad.webdev99@gmail.com>
2025-04-16 10:43:27 -0700

    business-and-featureCard-components-design

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import styles, { layout } from "../style";
+import { features } from "../constants";
+import Button from "./Button";
+import FeatureCard from "./FeatureCard";
 
 const Business = () => {
   return (
-    <div>Business</div>
-  )
-}
+    <section id="features" className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we'll handle
+          the money.
+        </h2>
 
-export default Business
+        <p className={`${styles.paragraph} max-w-[470px] mt-2`}>
+          with the right credit card, you can get the business funds you need to
+          succeed. We offer a range of credit card options to help you reach
+          your financial goals. Our credit cards are designed to provide you
+          with the flexibility and convenience you need to manage your business
+          finances.
+        </p>
+
+        <Button styles={"mt-10"} />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Business;
